bfd9061b2a6cc96aaf8ebec140ded02ae3211411
ORΔCLΣ <154529081+O-RA-CL-E@users.noreply.github.com>
2025-06-02 15:39:25 +0200

    Update in config.py

--- code/config.py
+++ code/config.py
@@ -1,18 +1,22 @@
-API_KEY = "TA_CLE_API"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("RIOT_API_KEY")
 
 PLATFORM = "euw1"
 REGION = "europe"
 
 DB_PARAMS = {
-    "dbname": "lol_db",
-    "user": "postgres",
-    "password": "ton_mot_de_passe",
-    "host": "localhost",
-    "port": 5432
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT")
 }
 
-# Timestamps Unix pour les périodes
 PERIODS = {
-    "2023-2024": (1680307200, 1711929600),  # 1 avril 2023 → 1 avril 2024
-    "2024-2025": (1711929600, 1743465600),  # 1 avril 2024 → 1 avril 2025
+    "2023-2024": (1680307200, 1711929600),
+    "2024-2025": (1711929600, 1743465600),
 }
